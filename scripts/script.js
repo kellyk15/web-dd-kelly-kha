37a79b8fc2f608be786539ebb3c260bc038a7412
kellyk15 <kellykha2004@gmail.com>
2026-02-05 10:16:21 +0100

    update

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var tinboxClosed = document.querySelector("section:nth-of-type(2) div:nth-of-type(1)");
+
+var tinboxOpen = document.querySelector("section:nth-of-type(2) div:nth-of-type(2)");
+
+deCheckbox.addEventListener('click', toggleVisibility);
+
+
+function toggleVisibility(){
+    if ()
+}
